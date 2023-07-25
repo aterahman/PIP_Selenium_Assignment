a99a14ecb815d14c9ba8455b600a9f52040137c8
aterahman <aterahman@deloitte.com>
2023-07-25 14:00:02 +0530

    Testng Listener file that will add to logger, report and take screen shot based on test outccome

---
+++ src/test/java/Utils/testnglistener.java
@@ -0,0 +1,51 @@
+package Utils;
+
+import Base.BaseTest;
+import com.aventstack.extentreports.Status;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.testng.TestListenerAdapter;
+
+import java.io.IOException;
+
+public class testnglistener  implements ITestListener
+{
+
+
+    @Override
+    public void onTestStart(ITestResult result)
+    {
+        BaseTest.log.info("Test has started"+result);
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result)
+    {
+        BaseTest.log.info("PASSED TEST "+result);
+
+        BaseTest.test.log(Status.PASS,"Passed test"+result);
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result)
+    {
+        BaseTest.log.info("FAILED TEST "+result);
+        BaseTest.test.log(Status.FAIL,"Failed Test"+result);
+        try
+        {
+            takingscreenshot.getscreenshotfortestfail();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result)
+    {
+        /* compiled code */
+    }
+}
